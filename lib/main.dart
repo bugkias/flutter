83a0d1965784c083a7731cfc33754d542b7c3b87
Bug Kias <n.mongkonkaha@gmail.com>
2018-08-12 23:55:38 +0700

    home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:basic_flutter/screens/home.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,12 +7,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Basic Flutter App',
-        home: Scaffold(
-          appBar: AppBar(title: Text('Home')),
-          body: Center(
-            child: Text('Welcome!'),
-          ),
-        ));
+      title: 'Basic Flutter App',
+      home: Home(),
+    );
   }
 }
