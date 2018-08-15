ac0be5b0f3b4bc88fa27a8014435eeef6bc1e57d
Bug Kias <n.mongkonkaha@gmail.com>
2018-08-15 16:01:45 +0700

    Column and Row Layouts

--- lib/screens/payment_screen.dart
+++ lib/screens/payment_screen.dart
@@ -8,8 +8,45 @@ class PaymentScreen extends StatefulWidget {
 class _PaymentScreenState extends State<PaymentScreen> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('หน้า 2 การชำระเงิน'),
+    return ListView(
+      children: <Widget>[
+        Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              height: 100.0,
+              color: Colors.redAccent,
+            ),
+            Container(
+              height: 200.0,
+              color: Colors.amberAccent,
+            ),
+            new Row(
+              children: <Widget>[
+                new Expanded(
+                  child: new Text('Object One', textAlign: TextAlign.center),
+                ),
+                new Expanded(
+                  child: new Container(
+                    height: 100.0,
+                    color: Colors.blue,
+                    child: Text('Test'),
+                  ),
+                ),
+                new Expanded(
+                  child: new FittedBox(
+                    fit: BoxFit.contain, // otherwise the logo will be tiny
+                    child: const FlutterLogo(),
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              child: Image(image: AssetImage('assets/images/melbourne.jpg')),
+            ),
+          ],
+        )
+      ],
     );
   }
 }
